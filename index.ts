f4e04082d7eba15b36d856adc282a2f0455cd931
purerosefallen <78877@qq.com>
2021-10-04 16:45:19 +0800

    fix

--- index.ts
+++ index.ts
@@ -3,4 +3,3 @@ export * from './src/koishi.decorators';
 export * from './src/koishi.interfaces';
 export * from './src/koishi.service';
 export * from './src/koishi.module';
-export * from 'koishi';
